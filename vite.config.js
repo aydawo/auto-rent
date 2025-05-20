65b05b754153bab1d5f3d0853b20784ea1693479
aydaa <aydaamozgar@gmail.com>
2025-05-20 17:26:30 +0330

    new-updatee

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,3 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/auto-rent/",
 });
-module.exports = {
-  content: ["./*.html", "./src/**/*.js"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
